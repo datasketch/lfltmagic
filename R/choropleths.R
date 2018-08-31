487d9b8e4afaf83d4ee34053d82dfb78673ef4b4
brazadas <anaherand@hotmail.com>
2018-08-31 11:40:26 -0400

    choropleth functions

--- R/choropleths.R
+++ R/choropleths.R
@@ -146,6 +146,7 @@ lflt_choropleth_GcdCat <- function(data,
     tidyr::drop_na(2)
 
   d <- d %>%
+    drop_na(1) %>%
     tidyr::replace_na(list(a = ifelse(is.character(d$a), "NA", NA),
                            b = ifelse(is.character(d$b), "NA", NA))) %>%
     dplyr::group_by(a, b) %>%
@@ -250,6 +251,7 @@ lflt_choropleth_GcdNum <- function(data,
     tidyr::drop_na(2)
 
   d <- d  %>%
+    drop_na(1) %>%
     tidyr::replace_na(list(a = ifelse(is.character(d$a), "NA", NA))) %>%
     dplyr::group_by(a) %>%
     dplyr::summarise(b = do.call(agg, list(b, na.rm = TRUE)))
@@ -345,7 +347,7 @@ lflt_choropleth_Gnm <- function(data,
                                 marks = c(",", "."),
                                 matchMethod = "lv",
                                 maxDist = 2,
-                                nBins = 8,
+                                nBins = 4,
                                 nDigits = 2,
                                 palette = c("#009EE3", "#E5007D", "#95C11E"),
                                 percentage = FALSE,
@@ -376,43 +378,35 @@ lflt_choropleth_Gnm <- function(data,
       alt <- geodata::geodataMeta(scope)$altnames %>%
         na.omit()
       a0 <- adist(d$a, alt$altname)
-      map(1:nrow(a0), function(u) {
-        s0 <- which(a0[u, ] == min(a0[u, ], na.rm = TRUE))
-
+      p0 <- map_df(1:nrow(a0), function(u) {
+        f0 <- which(a0[u, ] == min(a0[u, ], na.rm = TRUE))[1]
+        f1 <- unique(alt$id[f0])[1]
+        f2 <- grep(f1, dgeo@data$id)
+        if (sum(f2) > 0) {
+          bind_cols(dgeo@data[f2, ],
+                alt[f0, "altname"],
+                d[u, ],
+                data.frame("dist" = a0[u, f0]))
+        }
       })
-
-
-
-      # i0 <- map2_df(d$a, 1:length(d$a), function(u, e) {
-      #   j0 <- agrep(u, alt$altname, max.distance = maxDist, ignore.case = TRUE)[1]
-      #   j1 <- grep(alt$id[j0], dgeo@data$id)
-      #   if (!is.na(j0) > 0 & sum(j1) > 0) {
-      #     cbind(dgeo@data[j1, ], alt[j0, "altname"], d[e, ])
-      #   }
-      # })
-      # dgeo@data <- left_join(dgeo@data, i0, by = "id")
-      # dgeo@data <- d %>%
-      #   fuzzyjoin::stringdist_left_join(alt,
-      #                                   by = c(a = "altname"),
-      #                                   ignore_case = TRUE,
-      #                                   method = matchMethod,
-      #                                   max_dist = maxDist,
-      #                                   distance_col = "dist") %>%
-      #   dplyr::group_by(a) %>%
-      #   dplyr::filter(dist == min(dist)) %>%
-      #   dplyr::right_join(dgeo@data, by = "id")
-      # View(dgeo@data)
+      p1 <- p0 %>%
+        group_by(id) %>%
+        arrange(dist) %>%
+        slice(1)
+      dgeo@data <- left_join(dgeo@data, p1, by = "id")
+      names(dgeo@data) <- c("id", "name", "name_i", "altname", "a", "b", "dist")
     } else {
-      dgeo@data <- d %>%
-        fuzzyjoin::stringdist_right_join(dgeo@data,
-                                         by = c(a = "name"),
-                                         ignore_case = TRUE,
-                                         method = matchMethod,
-                                         max_dist = maxdist,
-                                         distance_col = "dist") %>%
-        dplyr::group_by(a) %>%
-        dplyr::filter(dist == min(dist)) %>%
-        dplyr::right_join(dgeo@data, by = c("name", "id"))
+      a0 <- adist(d$a, dgeo@data$name)
+      p0 <- map_df(1:nrow(a0), function(u) {
+        f0 <- which(a0[u, ] == min(a0[u, ], na.rm = TRUE))[1]
+        bind_cols(dgeo@data[f0, ], d[u, ], data.frame("dist" = a0[u, f0]))
+      })
+      p1 <- p0 %>%
+        group_by(id) %>%
+        arrange(dist) %>%
+        slice(1)
+      dgeo@data <- left_join(dgeo@data, p1, by = "id")
+      names(dgeo@data) <- c("id", "name", "a", "b", "dist")
     }
   } else {
     stop("Pick an available map for the 'scope' argument (geodata::availableGeodata)")
@@ -477,81 +471,116 @@ lflt_choropleth_Gnm <- function(data,
 #' @examples
 #' lflt_choropleth_GnmCat(sampleData("Gnm-Cat", nrow = 10))
 lflt_choropleth_GnmCat <- function(data,
-                                   palette = c("#009EE3", "#9B71AF", "#FFFF99"),
+                                   dropNa = FALSE,
                                    fillOpacity = 0.5,
-                                   #infoVar = NULL,
+                                   #format = c("", ""),
+                                   legendColor = "discrete",
+                                   legendPosition = "bottomleft",
+                                   legendTitle = NULL,
                                    label = NULL,
-                                   popup = NULL,
+                                   #marks = c(",", "."),
+                                   #nDigits = 2,
+                                   palette = c("#009EE3", "#9B71AF", "#FFFF99"),
+                                   #percentage = FALSE,
+                                   #infoVar = NULL,
+                                   # popup = NULL,
                                    scope = "world_countries",
                                    tiles = "CartoDB.Positron") {
   f <- fringe(data)
   nms <- getClabels(f)
-  popup <- popup %||% ""
+  d <- f$d
 
-  dd <- f$d %>%
-    na.omit() %>%
+  legendTitle <- legendTitle %||% ""
+
+
+  if (dropNa)
+    d <- d %>%
+    tidyr::drop_na(2)
+
+  d <- d %>%
+    drop_na(1) %>%
+    tidyr::replace_na(list(a = ifelse(is.character(d$a), "NA", NA),
+                           b = ifelse(is.character(d$b), "NA", NA))) %>%
     dplyr::group_by(a, b) %>%
     dplyr::summarise(c = n()) %>%
     dplyr::arrange(desc(c)) %>%
     dplyr::slice(1)
 
-
-  if (!is.null(scope) && scope %in% geodata::availableGeodata()) {
+  if (scope %in% geodata::availableGeodata()) {
     dgeo <- geojsonio::topojson_read(geodata::geodataTopojsonPath(scope))
     if ("altnames" %in% names(geodata::geodataMeta(scope))) {
       alt <- geodata::geodataMeta(scope)$altnames %>%
         na.omit()
-      dgeo@data <- dd %>%
-        fuzzyjoin::stringdist_left_join(alt,
-                                        by = c(a = "altname"),
-                                        ignore_case = TRUE,
-                                        method = "jw",
-                                        max_dist = 0.5,
-                                        distance_col = "dist") %>%
-        dplyr::group_by(a) %>%
-        dplyr::filter(dist == min(dist)) %>%
-        dplyr::right_join(dgeo@data, by = "id")
+      a0 <- adist(d$a, alt$altname)
+      p0 <- map_df(1:nrow(a0), function(u) {
+        f0 <- which(a0[u, ] == min(a0[u, ], na.rm = TRUE))[1]
+        f1 <- unique(alt$id[f0])[1]
+        f2 <- grep(f1, dgeo@data$id)
+        if (sum(f2) > 0) {
+          bind_cols(dgeo@data[f2, ],
+                alt[f0, "altname"],
+                d[u, ],
+                data.frame("dist" = min(a0[u, ], na.rm = TRUE)))
+        }
+      })
+      p1 <- p0 %>%
+        group_by(id) %>%
+        arrange(dist) %>%
+        slice(1)
+      dgeo@data <- left_join(dgeo@data, p1, by = "id")
+      names(dgeo@data) <- c("id", "name", "name_i", "altname", "a", "b", "c", "dist")
     } else {
-      dgeo@data <- dd %>%
-        fuzzyjoin::stringdist_right_join(dgeo@data,
-                                         by = c(a = "name"),
-                                         ignore_case = TRUE,
-                                         method = "jw",
-                                         max_dist = 0.5,
-                                         distance_col = "dist") %>%
-        dplyr::group_by(a) %>%
-        dplyr::filter(dist == min(dist)) %>%
-        dplyr::right_join(dgeo@data, by = c("name", "id"))
+      a0 <- adist(d$a, dgeo@data$name)
+      p0 <- map_df(1:nrow(a0), function(u) {
+        f0 <- which(a0[u, ] == min(a0[u, ], na.rm = TRUE))[1]
+        bind_cols(dgeo@data[f0, ], d[u, ], data.frame("dist" = a0[u, f0]))
+      })
+      p1 <- p0 %>%
+        group_by(id) %>%
+        arrange(dist) %>%
+        slice(1)
+      dgeo@data <- left_join(dgeo@data[, ], p1, by = "id")
+      names(dgeo@data) <- c("id", "name", "a", "b", "c", "dist")
     }
   } else {
     stop("Pick an available map for the 'scope' argument (geodata::availableGeodata)")
   }
 
-
   # los labels y popups
-  if (is.null(label) || !label %in% nms) {
-    #lab <- paste0(dgeo@data$id, ": ", dgeo@data$b)
-    lab <- map(as.list(1:nrow(dgeo@data)), function(r) {
-      htmltools::HTML(paste0("<b>", dgeo@data$name[r], "</b><br/>", nms[2], ": ", dgeo@data$b[r], "<br/>", dgeo@data$c[r]))
+  if (is.null(label)) {
+    lab <- map(1:nrow(dgeo@data), function(r) {
+      htmltools::HTML(paste0("<b>",
+                             dgeo@data$id[r],
+                             "</b><br/>",
+                             nms[2],
+                             ": <b>",
+                             dgeo@data$b[r],
+                             "</b>"))
     })
-  } else {
-    lab <- dgeo@data[[label]]
-  }
-  if (popup %in% nms) {
-    popup <- dgeo@data[[popup]]
   }
 
-  col <- colorNumeric(palette = palette, domain = NULL)
+  col <- colorFactor(palette = palette, domain = unique(dgeo@data$b))
 
   l <- leaflet(dgeo) %>%
     addProviderTiles(tiles) %>%
-    addPolygons(fillColor = col(dgeo@data$c),
-                label = lab,
-                popup = popup,
+    addPolygons(fillColor = col(dgeo@data$b),
                 fillOpacity = fillOpacity,
-                stroke = FALSE)
+                color = "black",
+                label = lab,
+                weight = 0.5)
+
+  if (legendColor == "discrete") {
+    l <- l %>%
+      addLegend(pal = col,
+                title = legendTitle,
+                values = dgeo@data$b,
+                opacity = 2,
+                position = legendPosition)
+
+  }
   l
 }
+
 
 
 #' Leaflet choropleths by numerical variable
@@ -566,76 +595,136 @@ lflt_choropleth_GnmCat <- function(data,
 #' @examples
 #' lflt_choropleth_GnmNum(sampleData("Gnm-Num", nrow = 10))
 lflt_choropleth_GnmNum <- function(data,
-                                   palette = c("#009EE3", "#9B71AF", "#FFFF99"),
-                                   fillOpacity = 0.5,
-                                   #infoVar = NULL,
-                                   label = NULL,
-                                   popup = NULL,
                                    agg = "sum",
+                                   dropNa = FALSE,
+                                   fillOpacity = 0.5,
+                                   format = c("", ""),
+                                   legendColor = "discrete",
+                                   legendPosition = "bottomleft",
+                                   legendTitle = NULL,
+                                   label = NULL,
+                                   marks = c(",", "."),
+                                   nBins = 4,
+                                   nDigits = 2,
+                                   palette = c("#009EE3", "#E5007D", "#95C11E"),
+                                   percentage = FALSE,
+                                   # popup = NULL,
+                                   #infoVar = NULL,,
                                    scope = "world_countries",
                                    tiles = "CartoDB.Positron") {
   f <- fringe(data)
   nms <- getClabels(f)
-  popup <- popup %||% ""
+  d <- f$d
 
-  dd <- f$d %>%
-    na.omit() %>%
+  legendTitle <- legendTitle %||% ""
+
+  if (dropNa)
+    d <- d %>%
+    tidyr::drop_na(2)
+
+  d <- d  %>%
+    drop_na(1) %>%
+    tidyr::replace_na(list(a = ifelse(is.character(d$a), "NA", NA))) %>%
     dplyr::group_by(a) %>%
     dplyr::summarise(b = do.call(agg, list(b, na.rm = TRUE)))
 
+  d <- percentColumn(d, "b", percentage, nDigits)
 
-  if (!is.null(scope) && scope %in% geodata::availableGeodata()) {
+  if (percentage & nchar(format[2]) == 0) {
+    format[2] <- "%"
+  }
+
+  if (scope %in% geodata::availableGeodata()) {
     dgeo <- geojsonio::topojson_read(geodata::geodataTopojsonPath(scope))
     if ("altnames" %in% names(geodata::geodataMeta(scope))) {
       alt <- geodata::geodataMeta(scope)$altnames %>%
         na.omit()
-      dgeo@data <- dd %>%
-        fuzzyjoin::stringdist_left_join(alt,
-                                        by = c(a = "altname"),
-                                        ignore_case = TRUE,
-                                        method = "jw",
-                                        max_dist = 0.5,
-                                        distance_col = "dist") %>%
-        dplyr::group_by(a) %>%
-        dplyr::filter(dist == min(dist)) %>%
-        dplyr::right_join(dgeo@data, by = "id")
+      a0 <- adist(d$a, alt$altname)
+      p0 <- map_df(1:nrow(a0), function(u) {
+        f0 <- which(a0[u, ] == min(a0[u, ], na.rm = TRUE))[1]
+        f1 <- unique(alt$id[f0])[1]
+        f2 <- grep(f1, dgeo@data$id)
+        if (sum(f2) > 0) {
+          bind_cols(dgeo@data[f2, ],
+                    alt[f0, "altname"],
+                    d[u, ],
+                    data.frame("dist" = min(a0[u, ], na.rm = TRUE)))
+        }
+      })
+      p1 <- p0 %>%
+        group_by(id) %>%
+        arrange(dist) %>%
+        slice(1)
+      dgeo@data <- left_join(dgeo@data, p1, by = "id")
+      names(dgeo@data) <- c("id", "name", "name_i", "altname", "a", "b", "dist")
     } else {
-      dgeo@data <- dd %>%
-        fuzzyjoin::stringdist_right_join(dgeo@data,
-                                         by = c(a = "name"),
-                                         ignore_case = TRUE,
-                                         method = "jw",
-                                         max_dist = 0.5,
-                                         distance_col = "dist") %>%
-        dplyr::group_by(a) %>%
-        dplyr::filter(dist == min(dist)) %>%
-        dplyr::right_join(dgeo@data, by = c("name", "id"))
+      a0 <- adist(d$a, dgeo@data$name)
+      p0 <- map_df(1:nrow(a0), function(u) {
+        f0 <- which(a0[u, ] == min(a0[u, ], na.rm = TRUE))[1]
+        bind_cols(dgeo@data[f0, ], d[u, ], data.frame("dist" = a0[u, f0]))
+      })
+      p1 <- p0 %>%
+        group_by(id) %>%
+        arrange(dist) %>%
+        slice(1)
+      dgeo@data <- left_join(dgeo@data[, ], p1, by = "id")
+      names(dgeo@data) <- c("id", "name", "a", "b", "dist")
     }
   } else {
     stop("Pick an available map for the 'scope' argument (geodata::availableGeodata)")
   }
 
   # los labels y popups
-  if (is.null(label) || !label %in% nms) {
+  if (is.null(label)) {
     #lab <- paste0(dgeo@data$id, ": ", dgeo@data$b)
-    lab <- map(as.list(1:nrow(dgeo@data)), function(r) {
-      htmltools::HTML(paste0("<b>", dgeo@data$name[r], "</b><br/>", nms[2], ": ", dgeo@data$b[r]))
+    lab <- map(1:nrow(dgeo@data), function(r) {
+      htmltools::HTML(paste0(dgeo@data$id[r],
+                             ": <b>",
+                             format[1],
+                             format(dgeo@data$b[r], big.mark = marks[1], decimal.mark = marks[2]),
+                             ifelse(is.na(dgeo@data$b[r]), "", format[2]),
+                             "<br/>"))
     })
-  } else {
-    lab <- dgeo@data[[label]]
-  }
-  if (popup %in% nms) {
-    popup <- dgeo@data[[popup]]
   }
 
-  col <- colorNumeric(palette = palette, domain = NULL)
+  # colorBin en vez de colorNumeric
+  # col <- colorQuantile(palette = palette,
+  #                      domain = dgeo@data$b,
+  #                      probs = seq(0, 1, 1/nBins))
+
+  col <- colorNumeric(palette = palette,
+                      domain = dgeo@data$b)
+  if (legendColor == "discrete") {
+    col <- colorBin(palette = palette,
+                    domain = dgeo@data$b,
+                    bins = nBins,
+                    pretty = FALSE)
+  }
+
+  # col <- colorNumeric(palette = palette, domain = NULL)
 
   l <- leaflet(dgeo) %>%
     addProviderTiles(tiles) %>%
     addPolygons(fillColor = col(dgeo@data$b),
-                label = lab,
-                popup = popup,
+                # addPolygons(fillColor = col(unique(dgeo@data$b)),
                 fillOpacity = fillOpacity,
-                stroke = FALSE)
+                color = "black",
+                label = lab,
+                weight = 0.5)
+  # stroke = FALSE)
+
+  if (legendColor %in% c("discrete", "continuous")) {
+    l <- l %>%
+      addLegend(pal = col,
+                title = legendTitle,
+                values = dgeo@data$b,
+                labFormat = labFor(prefix = format[1],
+                                   suffix = format[2],
+                                   big.mark = marks[1],
+                                   decimal.mark = marks[2],
+                                   digits = nDigits),
+                opacity = 2,
+                position = legendPosition)
+  }
   l
 }
