c01a4b2f84f66f90df119d50355aa745cb68e955
Camila Achury <camilaachury.4@gmail.com>
2021-04-28 23:26:14 -0300

    add choropleth GcdNum documentation

--- R/choropleth_GcdNum.R
+++ R/choropleth_GcdNum.R
@@ -1,15 +1,26 @@
 
-#' Leaflet choropleths by numerical variable
+#' choropleth chart Gcd Num
 #'
-#' Leaflet choropleths by numerical variable
+#' @description
+#' `lflt_choropleth_GcdNum()` Create a Leaflet choropleth map based on a particular data type.
+#' In this case, you can load data with only two columns, where the firts it's a **geocode column**,
+#' and the second is a **numeric class column**, or make sure that the first two columns of
+#' your data meet this condition
 #'
-#' @name lflt_choropleth_GcdNum
-#' @param x A data.frame
-#' @return leaflet viz
-#' @section ctypes: Gcd-Num
 #' @export
+#' @family Gcd-Num plots
+#' @section Ftype:
+#' Gcd-Num
 #' @examples
-#' lflt_choropleth_GcdNum(sampleData("Gcd-Num", nrow = 10))
+#' data <- sample_data("Gcd-Num", n = 30)
+#' lflt_choropleth_GcdNum(data)
+#'
+#'
+#' # if you want to calculate the average instead of the sum, you can use agg inside a function
+#' lflt_choropleth_GcdNum(data,
+#'                        agg = "mean")
+#'
+#'
 lflt_choropleth_GcdNum <- function(data = NULL, ...) {
 
   if (!is.null(data)) {
@@ -22,7 +33,6 @@ lflt_choropleth_GcdNum <- function(data = NULL, ...) {
 
   lf <- lflt_basic_choropleth(l) %>%
     lflt_background(l$theme) %>%
-    #lflt_bounds(l$b_box) %>%
     lflt_graticule(l$graticule) %>%
     lflt_titles(l$titles)
 
@@ -30,15 +40,28 @@ lflt_choropleth_GcdNum <- function(data = NULL, ...) {
 }
 
 
-#' Leaflet choropleths by numerical variable
+#' choropleth chart Gcd
 #'
-#' @name lflt_choropleth_Gcd
-#' @param data A data.frame
-#' @return leaflet viz
-#' @section ctypes: Gcd
+#' @description
+#' `lflt_choropleth_Gcd()` Create a Leaflet choropleth map based on a particular data type.
+#' In this case, you can load data with only one column, where it's a **geocode column**,
+#' or make sure that the first column of your data meet this condition
+#'
 #' @export
+#' @inheritParams lflt_choropleth_GcdNum
+#' @section Ftype:
+#' Gcd
 #' @examples
-#' lflt_choropleth_Gcd(sampleData("Gcd", nrow = 10))
+#' data <- sample_data("Gcd-Num", n = 30)
+#' lflt_choropleth_Gcd(data)
+#'
+#' # Activate data labels
+#' lflt_choropleth_Gcd(data)
+#'
+#' # if you want to calculate the average instead of the sum, you can use agg inside a function
+#' lflt_choropleth_Gcd(data,
+#'                        agg = "mean")
+#'
 lflt_choropleth_Gcd <-  function(data = NULL, ...) {
 
   if (!is.null(data)) data[[1]] <- as_Gcd(data[[1]])
@@ -48,7 +71,6 @@ lflt_choropleth_Gcd <-  function(data = NULL, ...) {
 
   lf <- lflt_basic_choropleth(l) %>%
     lflt_background(l$theme) %>%
-    #lflt_bounds(l$b_box) %>%
     lflt_graticule(l$graticule) %>%
     lflt_titles(l$titles)
 
