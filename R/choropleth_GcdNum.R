a10904404cb3f20e3f91610f9e37f9dcbaf7531a
Lena Mangold <lena@mangold.org>
2021-05-27 11:28:39 -0500

    add homodatum namespaces to as_ fcts

--- R/choropleth_GcdNum.R
+++ R/choropleth_GcdNum.R
@@ -24,8 +24,8 @@
 lflt_choropleth_GcdNum <- function(data = NULL, ...) {
 
   if (!is.null(data)) {
-    data[[1]] <- as_Gcd(data[[1]])
-    data[[2]] <- as_Num(data[[2]])
+    data[[1]] <- homodatum::as_Gcd(data[[1]])
+    data[[2]] <- homodatum::as_Num(data[[2]])
   }
   opts <- dsvizopts::merge_dsviz_options(...)
 
