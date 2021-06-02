4bb6cbdb744e47b941f923b7d1d077f0f7f4e589
Camila Achury <camilaachury.4@gmail.com>
2021-06-02 15:28:26 -0300

    change dsvizopts by dsvizprep and transfer tooltip to dsvizprep package

--- R/helpers.R
+++ R/helpers.R
@@ -25,48 +25,7 @@ lflt_palette <- function(opts) {
   do.call(color_mapping, l)
 }
 
-#' @export
-lflt_tooltip <- function(nms, label_ftype = NULL, tooltip) {
-  if (is.null(nms)) stop("Enter names")
 
-  if(grepl("\\{name\\}", tooltip)) {
-    nm <- names(nms)
-    nms <- c(nms, "name")
-    names(nms) <- c(nm, "name")
-  }
-
-  nms <- gsub("[][!()*`|]", "",nms)
-  nms <- gsub("[\r\n]", " ", nms)
-  label_ftype_clean <- gsub("[][!()*`|{}]", "", label_ftype)
-  label_ftype_clean <- gsub("[\r\n]", " ", label_ftype_clean)
-  nms_names <- names(nms)
-
-  if (tooltip == "") tooltip <- NULL
-  if (is.null(tooltip)) {
-    tooltip  <- paste0(purrr::map(seq_along(label_ftype), function(i) {
-      paste0(label_ftype[i], ": {", label_ftype_clean[i], "}")
-    }) %>% unlist(), collapse = "<br/>")
-  } else {
-    tooltip <- gsub("[][()*`|]", "", tooltip)#gsub("[][!#$()*,.:;<=>@^`|~.", "", tooltip)
-  }
-
-  points <- gsub("\\{|\\}", "",
-                 stringr::str_extract_all(tooltip, "\\{.*?\\}")[[1]])
-
-  if(!all(points %in% nms)) stop("all variables within braces must be contained in the loaded data")
-
-  if (identical(points, character())) {
-    tooltip <- tooltip
-  } else {
-    l <- purrr::map(seq_along(points), function(i){
-
-      true_points <-  paste0("{",names(nms[match(points[i], nms)]),"_label}")
-      tooltip <<- gsub(paste0("\\{",points[i], "\\}"), true_points, tooltip)
-    })[[length(points)]]
-  }
-
-  tooltip
-}
 
 
 agg_tooltip <- function(data, label_by, nms, label_ftype, tooltip) {
@@ -75,7 +34,7 @@ agg_tooltip <- function(data, label_by, nms, label_ftype, tooltip) {
     dplyr::mutate(labels = ifelse(is.na(a),
                                   glue::glue(paste0("<span style='font-size:13px;'><strong>{", label_by,"_label}</strong></span>")) %>% lapply(htmltools::HTML),
                                   glue::glue(
-                                    lflt_tooltip(nms = nms,
+                                    dsvizprep::tooltip_map(nms = nms,
                                                  label_ftype = label_ftype,
                                                  tooltip = tooltip)) %>%
                                     lapply(htmltools::HTML))
--- R/lfltmagic_prep.R
+++ R/lfltmagic_prep.R
@@ -2,7 +2,7 @@
 #' @export
 lfltmagic_prep <- function(data = NULL, opts = NULL, by_col = "name", ftype="Gnm-Num", ...) {
   # call geographical info
-  shape <- dsvizopts::shape_info(map_name = opts$extra$map_name,
+  shape <- dsvizprep::shape_info(map_name = opts$extra$map_name,
                                  ftype = ftype,
                                  by_col = by_col,
                                  addRds = FALSE)
@@ -11,14 +11,14 @@ lfltmagic_prep <- function(data = NULL, opts = NULL, by_col = "name", ftype="Gnm
   topoInfo$labels <- topoInfo[[by_col]]
   # data preparation by type
   if (!is.null(data)) {
-    list_d <- dsvizopts::data_map_prep(data = data,
+    list_d <- dsvizprep::data_map_prep(data = data,
                                        ftype = ftype,
                                        agg =  opts$summarize$agg,
                                        color_by = opts$style$color_by,
                                        more_levels = shape$more_levels,
                                        ptage_col = opts$postprocess$percentage_col)
     # format setting of data being displayed
-    data_format <- dsvizopts::format_prep(data = list_d$data,
+    data_format <- dsvizprep::format_prep(data = list_d$data,
                                           dic = list_d$dic,
                                           formats = list(sample_num = opts$style$format_sample_num,
                                                          sample_cat = opts$style$format_sample_cat,
